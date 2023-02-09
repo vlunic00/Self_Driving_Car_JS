2c81df760581de832769a75762859fd81b67d4ac
vlunic00 <vlunic00@fesb.hr>
2023-02-09 17:38:37 +0100

    Added traffic and built the neural network

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ function lerp(A,B,t){
 function getIntersection(A,B,C,D){
     const tTop=(D.x-C.x)*(A.y-C.y)-(D.y-C.y)*(A.x-C.x);
     const uTop=(C.y-A.y)*(A.x-B.x)-(C.x-A.x)*(A.y-B.y);
-    const bottom=(D.y-C.y)*(B.x-A.x)-(D.x-C.x)*(B.x-A.y);
+    const bottom=(D.y-C.y)*(B.x-A.x)-(D.x-C.x)*(B.y-A.y);
 
     if(bottom!=0){
         const t=tTop/bottom;
@@ -20,4 +20,21 @@ function getIntersection(A,B,C,D){
     }
 
     return null;
+}
+
+function polyIntersect(poly1, poly2){
+    for(let i = 0; i<poly1.length; i++){
+        for(let j = 0; j<poly2.length; j++){
+            const touch = getIntersection(
+                poly1[i],
+                poly1[(i+1) % poly1.length],
+                poly2[j],
+                poly2[(j+1) % poly2.length] 
+            );
+            if(touch){
+                return true;
+            }
+        }
+    }
+    return false;
 }
